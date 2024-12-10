ca768bda5568b027fb07103ce6a4c606ab98c1a4
Ali Hassan <alhassan2903@gmail.com>
2024-12-10 16:35:51 +0500

    Some Basic changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { varsIgnorePattern: '^_', caughtErrors: 'none' },
-    ],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows modern syntax
+    sourceType: 'module', // Allows ES Modules
+    ecmaFeatures: {
+      jsx: true, // Allows JSX syntax
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the React version
+    },
   },
 };
